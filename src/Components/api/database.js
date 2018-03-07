8cc5a1a3bbd97805fee12878aa72699a56629642
Piotrek <phiwanczuk@gmail.com>
2018-03-07 19:13:46 +0100

    database added

---
+++ src/Components/api/database.js
@@ -0,0 +1,228 @@
+var quizQuestions = [
+    {
+        question: "Która drużyna ma na swoim koncie najwiecęj zwycięstw w Lidze Mistrzów?",
+        answers: [
+            {
+
+                content: "Chealse"
+            },
+            {
+
+                content: "Real Madryt"
+            },
+            {
+
+                content: "AC Milan"
+            },
+            {
+
+                content: "Bayern"
+            }
+
+
+
+
+        ]
+    },
+    {
+        question: "Kto był bohaterem najdroższego transferu w historii?",
+        answers: [
+            {
+
+                content: "Neymar JR."
+            },
+            {
+
+                content: "Cristiano Ronaldo"
+            },
+            {
+
+                content: "Lionel Messi"
+            },
+            {
+
+                content: "Jakub Wawrzyniak"
+            }
+
+
+
+
+        ]
+    },
+    {
+        question: "Kto strzelił najwięcej bramek dla Reprezntacji Polski w Piłkę Nożną?",
+        answers: [
+            {
+
+                content: "Robert Lewandowski"
+            },
+            {
+
+                content: "Sławomir Peszko"
+            },
+            {
+
+                content: "Włodzimierz Lubański"
+            },
+            {
+
+                content: "Grzegorz Lato"
+            }
+
+        ]
+    },
+    {
+        question: "Kto strzelił najwięcej bramek na Mistrzostwach Świata w Piłkę Nożną?",
+        answers: [
+            {
+
+                content: "Gerd Muller"
+            },
+            {
+
+                content: "Pele"
+            },
+            {
+                content: "Miroslav Klose"
+            },
+            {
+                content: "Ronaldo"
+            }
+
+        ]
+    },
+    {
+        question: "W jakim klubie grał Cristiano Ronaldo przed przyjściem do Realu Madryt?",
+        answers: [
+            {
+
+                content: "Manchaster United"
+            },
+            {
+
+                content: "Barcelona"
+            },
+            {
+
+                content: "Chealse"
+            },
+            {
+
+                content: "Juventus"
+            }
+
+        ]
+    },
+    {
+        question: "Jakie drużyn wzięły udział w meczu, w którym padł najwyższy wynik w historii Ligi Mistrzów?",
+        answers: [
+            {
+
+                content: "Legia Warszawa vs Borussia Dortmund"
+            },
+            {
+
+                content: "Real Madryt vs PAOK Saloniki"
+            },
+            {
+
+                content: "Manchester City vs FC Basel"
+            },
+            {
+
+                content: "FC Barcelona vs PSG"
+            }
+
+        ]
+    },
+    {
+        question: "Który zespół może się poszczycić najwiekszą liczbą zwycięstw w angielskiej Premier League?",
+        answers: [
+            {
+
+                content: "Manchester City"
+            },
+            {
+
+                content: "Arsenal"
+            },
+            {
+
+                content: "Aston Villa"
+            },
+            {
+
+                content: "Manchester United"
+            }
+
+        ]
+    },
+    {
+        question: "Kto był zwycięzcą Mistrzostw Europy w 2012 roku?",
+        answers: [
+            {
+
+                content: "Niemcy"
+            },
+            {
+
+                content: "Włochy"
+            },
+            {
+
+                content: "Hiszpania"
+            },
+            {
+
+                content: "San Marino"
+            }
+
+        ]
+    },
+    {
+        question: "Jaki angielski zespół nosi przydomek The Invincibles, odnoszący się do jednego z sezonów, w którym zespół ten nie przegrał ani jednego meczu?",
+        answers: [
+            {
+
+                content: "Nottingham Forest"
+            },
+            {
+
+                content: "Liverpool"
+            },
+            {
+
+                content: "Tottenham"
+            },
+            {
+
+                content: "Arsenal"
+            }
+
+        ]
+    },
+    {
+        question: "Jakiego piłkarza znokautował Zinedine Zidane podczas Mistrzostw Świata w Niemczech w 2006?",
+        answers: [
+            {
+
+                content: "Materazzi"
+            },
+            {
+
+                content: "Barzagli"
+            },
+            {
+
+                content:  "Cannavaro"
+            },
+            {
+
+                content: "Zambrotta"
+            }
+
+        ]
+    },
+];
+
+export default quizQuestions;
